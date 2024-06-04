09c311f0b0f63280bfbffad48495c9c1d4ab19fc
Shaina pathania <98539629+shainapathania@users.noreply.github.com>
2024-06-04 18:40:38 +0000

    added

---
+++ 01_basics/03_variableConversion.js
@@ -0,0 +1,29 @@
+let score = "33ab"    //string value
+
+console.log(score);
+console.log(typeof score);
+
+
+let scoreInNumber = Number(score);
+console.log(scoreInNumber);
+console.log(typeof scoreInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+//  null => 0
+// undefined=>undefined
+//  true => 1; false => 0
+
+let isLoggedIn = "shaina"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+ console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "shaina" => true
+
+let player=2
+let playerName=String(player)
+console.log(typeof playerName);
